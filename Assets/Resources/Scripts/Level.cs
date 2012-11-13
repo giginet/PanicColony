cba7a872dd6bec1f2959ca8bbfc4ecf3f70d6244
giginet <giginet.net@gmail.com>
2012-11-14 05:32:14 +0900

    Roomクラス実装。ステージデータから部屋をパースできるようにした

--- Assets/Resources/Scripts/Level.cs
+++ Assets/Resources/Scripts/Level.cs
@@ -3,34 +3,57 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Level {
-	private Dictionary<Vector2, char> map;
-	private int level = 0;
-	private int width = 0;
-	private int height = 0;	
-	
-	public Level (int level, int width, int height, Dictionary<Vector2, char> map) {
-		this.map = map;
-		this.width = width;
-		this.height = height;
-	}
-	
-	public char GetMap (int x, int y) {
-		Vector2 v = new Vector2(x, y);
-		if (this.map.ContainsKey(v) ) {
-			return this.map[v];
-		}
-		return ' ';
-	}
-	
-	public int GetLevel () {
-		return this.level;
-	}
-	
-	public int GetWidth () {
-		return this.width;
-	}
-	
-	public int GetHeight () {
-		return this.height;
-	}
+    private Dictionary<Vector2, char> map;
+    private List<Room> rooms;
+    private int level = 0;
+    private int width = 0;
+    private int height = 0;    
+    
+    public Level (int level, int width, int height, Dictionary<Vector2, char> map) {
+        this.map = map;
+        this.width = width;
+        this.height = height;
+        this.rooms = new List<Room>();
+    }
+    
+    public char GetMap (int x, int y) {
+        Vector2 v = new Vector2(x, y);
+        if (this.map.ContainsKey(v) ) {
+            return this.map[v];
+        }
+        return ' ';
+    }
+    
+    public int GetLevel () {
+        return this.level;
+    }
+    
+    public int GetWidth () {
+        return this.width;
+    }
+    
+    public int GetHeight () {
+        return this.height;
+    }
+    
+    public void AddRoom (Room room) {
+        this.rooms.Add(room);
+        Debug.Log (rooms.Count);
+    }
+    
+    public bool IsFloor (int x, int y) {
+        char c = this.GetMap(x, y);
+        char[] floorList = {'.', 'S'};
+        List<char> floors = new List<char>(floorList);
+        return floors.Contains(c); 
+    }
+    
+    public bool ContainsInRooms (int x, int y) {
+        foreach (Room room in this.rooms) {
+            if ( room.ContainsFloor(x, y) ) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- Assets/Resources/Scripts/Room.cs
+++ Assets/Resources/Scripts/Room.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Room {
+    private List<Vector2> floors;
 
-	Room () {
-	}
-
+    public Room () {
+        this.floors = new List<Vector2>();
+    }
+    
+    public bool ContainsFloor (int x, int y) {
+        Vector2 p = new Vector2(x, y);
+        return this.floors.Contains(p);
+    }
+    
+    public void AddFloor(int x, int y) {
+        Vector2 p = new Vector2(x, y);
+        this.floors.Add(p);
+    }
 }
